ecac9b117b36fb6e81925bdd18048d370831767c
Satyajeet Rout <satyajeetrout98@gmail.com>
2024-09-14 18:30:48 +0530

    first commit

---
+++ server/helpers/paypal.js
@@ -0,0 +1,11 @@
+const dotenv = require('dotenv')
+dotenv.config()
+const paypal = require("paypal-rest-sdk");
+
+paypal.configure({
+  mode: process.env.PAYPAL_Mode,
+  client_id: process.env.PAYPAL_CLIENT_ID,
+  client_secret: process.env.PAYPAL_CLIENT_SECRET,
+});
+
+module.exports = paypal;
